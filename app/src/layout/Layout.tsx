3d3e2e75e1070851d4e21536cdc275cc225c9374
vufimcev <buktor@bk.ru>
2026-03-22 08:27:17 -0700

    fix: show loader on page transitions via Suspense key

--- app/src/layout/Layout.tsx
+++ app/src/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import type { FC } from 'react'
 import { Suspense } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import styles from './Layout.module.css'
 import { Header } from './Header'
 import { Sidebar } from './Sidebar'
 import { Loader } from '../ui'
 
 export const Layout: FC = () => {
+  const location = useLocation()
+
   return (
     <div className={`${styles.pageContainer} min-h-screen bg-app-bg text-app-text`}>
       <Header />
@@ -14,7 +16,7 @@ export const Layout: FC = () => {
         <Sidebar />
         <main className={styles.main}>
           <div className={`${styles.contentContainer} flex-1 py-6`}>
-            <Suspense fallback={<Loader />}>
+            <Suspense key={location.pathname} fallback={<Loader />}>
               <Outlet />
             </Suspense>
           </div>
